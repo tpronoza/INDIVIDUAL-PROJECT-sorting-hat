5bc7e3b9bc3fcd637e38de3c244cf6938bcf67a1
Tatiana Pronoza <tpronoza1@gmail.com>
2022-05-02 18:07:37 -0500

    created setup branch with all the files

---
+++ data/reference.js
@@ -0,0 +1,80 @@
+export const referenceList = [
+  {
+    id: 1,
+    name: "Harry Potter",
+    house: "Gryffindor",
+    status: "Active"
+  },
+  {
+    id: 2,
+    name: "Hermione Granger",
+    house: "Gryffindor",
+    status: "Active"
+  },
+  {
+    id: 3,
+    name: "Ron Weasley",
+    house: "Gryffindor",
+    status: "Active"
+  },
+  {
+    id: 4,
+    name: "Ginny Weasley",
+    house: "Gryffindor",
+    status: "Active"
+  },
+  {
+    id: 5,
+    name: "Fred Weasley",
+    house: "Gryffindor",
+    status: "Active"
+  },
+  {
+    id: 6,
+    name: "George Weasley",
+    house: "Gryffindor",
+    status: "Active"
+  },
+  {
+    id: 7,
+    name: "Draco Malfoy",
+    house: "Slytherin",
+    status: "Active"
+  },
+  {
+    id: 8,
+    name: "Luna Lovegood",
+    house: "Ravenclaw",
+    status: "Active"
+  },
+  {
+    id: 9,
+    name: "Neville Longbottom",
+    house: "Gryffindor",
+    status: "Active"
+  },
+  {
+    id: 10,
+    name: "Cho Chang",
+    house: "Ravenclaw",
+    status: "Active"
+  },
+  {
+    id: 11,
+    name: "Cedric Diggory",
+    house: "Hufflepuff",
+    status: "Active"
+  },
+  {
+    id: 12,
+    name: "Albus Dumbledore",
+    house: "Gryffindor",
+    status: "Active"
+  },
+  {
+    id: 13,
+    name: "Tom Riddle",
+    house: "Slytherin",
+    status: "Expelled"
+  },
+];
